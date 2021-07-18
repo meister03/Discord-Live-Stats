c6e204177af5945bfee53298d876e1a14a7203a9
meister03 <rpisakib@gmail.com>
2021-07-18 12:53:39 +0200

    ADD Data Post Option

--- zindex.js
+++ zindex.js
@@ -1,6 +1,7 @@
 const config = require("./config.json")
-
+const Passport = require("discord-passport");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.listen(3000, () => {
@@ -8,7 +9,8 @@ app.listen(3000, () => {
 });
 
 app.use(express.static(__dirname + '/'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const posts = [
   {
@@ -32,20 +34,34 @@ const posts = [
     message: 'Heartbeat received',
   },
 ]
-
+const data = {status: 'Online', cpu: `${(Math.random()*3).toFixed(2)}%`, ram: `${getRamUsageinMB()} MB`, ping: `20 ms`, servercount: `1000 Guilds`, color: `green` , start: `20m`};
 
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname);
 
-app.get("/start", (req, res) => {
-  const data = {status: 'Online', cpu: `${(Math.random()*3).toFixed(2)}%`, ram: `${getRamUsageinMB()} MB`, ping: `20 ms`, servercount: `1000 Guilds`, color: `green` , start: `20m`};
-  res.render("starter", {posts: posts, config: config, data: data});
 
+app.get('/',(req, res) => {
+  res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${config.bot.client_id}&redirect_uri=${config.redirect_uri}&response_type=code&scope=${config.scope.join('+')}`)
+})
 
-});
-
+app.get('/login', async (req, res) => {
+  const code = req.query.code;
+  const passport = new Passport({
+      code: code,
+      client_id: config.bot.client_id,
+      client_secret: config.bot.client_secret,
+      redirect_uri: config.redirect_uri,
+      scope: config.scope
+  })
+  await passport.open(); // Trades your code for an access token and gets the basic scopes for you.
+  if(config.owners.includes(passport.user.id)){
+    res.render("starter", {posts: posts, config: config, data: data});
+  }else{
+    return res.end(`Access denied!`);
+  }
+})
 
 let i = 0;
 app.get("/status", (req, res) =>{
@@ -70,30 +86,7 @@ app.get("/status", (req, res) =>{
       message: `Heartbeat received ${msg}`,
       ram: `${getRamUsageinMB()} MB`
     },
-    {
-      id: 1,
-      status: status,
-      message: msg2,
-      ram: `${getRamUsageinMB()} MB`
-    },
-    {
-      id: 2,
-      status: 'offline',
-      message: `No Heartbeat | Disconnected `,
-      ram: `${getRamUsageinMB()} MB`
-    },
-    {
-      id: 3,
-      status: 'online',
-      message: `Heartbeat received ${msg}`,
-      ram: `${getRamUsageinMB()} MB`,
-    },
-    {
-      id: 4,
-      status: 'online',
-      message: `Heartbeat received ${msg}`,
-      ram:  `${getRamUsageinMB()} MB`,
-    },
+   
   ]
     res.send(newposts);
     if(i === 10) i =0;
@@ -105,6 +98,11 @@ app.get("/shard", (req, res) => {
   const shardid = req.query.shardid;
   res.send({status: 'Online', cpu: `${(Math.random()*3).toFixed(2)}%`, ram: `${getRamUsageinMB()} MB`, ping: `20 ms`, servercount: `1000 Guilds` })
   return;
+})
+
+
+app.post('/stats', (req, res) => {
+  console.log(req.body)
 })
 
 
