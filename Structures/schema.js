288fc5d861b36ca8777b986c12a38e1e3081df06
meister03 <69507874+meister03@users.noreply.github.com>
2021-08-01 08:59:20 +0200

    Update schema.js
    downgrade to node v12

--- Structures/schema.js
+++ Structures/schema.js
@@ -1,13 +1,13 @@
 class Schema{
     constructor(data){
-        this.id = data.id ?? `NG`;
-        this.status = data.status ?? 5;
-        this.cpu = data.cpu ?? NaN;
-        this.ram = data.ram ?? NaN;
-        this.ping = data.ping ?? NaN;
-        this.message =  data.message ?? `No Data Recieved`;
-        this.guildcount = data.guildcount ?? NaN;
-        this.upsince = data.upsince ?? 0;
+        this.id = data.id || `NG`;
+        this.status = data.status || 5;
+        this.cpu = data.cpu || NaN;
+        this.ram = data.ram || NaN;
+        this.ping = data.ping || NaN;
+        this.message =  data.message || `No Data Recieved`;
+        this.guildcount = data.guildcount || NaN;
+        this.upsince = data.upsince || 0;
     }
     toObject(){
         return this;
