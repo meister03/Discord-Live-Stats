a9b3eec93e8936ed3364d608fad31ae8aaf9ce11
meister03 <69507874+meister03@users.noreply.github.com>
2021-08-04 15:17:31 +0200

    Update schema.js
    fix falsy values

--- Structures/schema.js
+++ Structures/schema.js
@@ -1,13 +1,13 @@
 class Schema{
-    constructor(data){
-        this.id = data.id || `NG`;
-        this.status = data.status || 5;
-        this.cpu = data.cpu || NaN;
-        this.ram = data.ram || NaN;
-        this.ping = data.ping || NaN;
+    constructor(data = {}){
+        this.id = isNaN(data.id) ? `NG` : data.id
+        this.status = isNaN(data.status) ? 5 : data.status;
+        this.cpu = isNaN(data.cpu) ? NaN : data.cpu;
+        this.ram = isNaN(data.ram) ? NaN : data.ram;
+        this.ping =  isNaN(data.ping) ? NaN : data.ping;
         this.message =  data.message || `No Data Recieved`;
-        this.guildcount = data.guildcount || NaN;
-        this.upsince = data.upsince || 0;
+        this.guildcount = isNaN(data.guildcount) ? NaN : data.guildcount;
+        this.upsince =  isNaN(data.upsince) ? 0 : data.upsince;
     }
     toObject(){
         return this;
